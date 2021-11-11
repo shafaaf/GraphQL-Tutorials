68c550e5b4c68c6b5341396fab5f115e4a96585c
shafaaf <shafaaf.hossain@hotmail.com>
2021-11-11 17:43:13 -0500

    jksbfg

---
+++ graphql-beginner-series/README.md
@@ -0,0 +1,3 @@
+## Graphql basics series using Apollo server:
+
+https://www.youtube.com/watch?v=DyvsMKsEsyE&list=PLN3n1USn4xln0j_NN9k4j5hS1thsGibKi&ab_channel=BenAwad
---
+++ graphql-beginner-series/src/index.js
@@ -0,0 +1,17 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello Worldz!"
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => console.log(`server started at ${url}`));
